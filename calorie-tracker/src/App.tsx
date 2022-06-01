6b9a777ab878efeb4abc37c0df49130f593413b7
Pokrzw <paulinab2014@interia.pl>
2022-06-01 18:29:03 +0200

    react router works

--- calorie-tracker/src/App.tsx
+++ calorie-tracker/src/App.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import './App.css';
 import LoginForm from './components/LoginForm';
+import { BrowserRouter,Routes, Route } from 'react-router-dom';
 
 
 function App() {
   return (
-  <div className="a">
-    <LoginForm/>
-  </div>
+<BrowserRouter>
+<Routes>
+  <Route path='/' element={<LoginForm/>} />
+</Routes>
+</BrowserRouter>
+  // <div className="a">
+  //   <LoginForm/>
+  // </div>
   );
 }
 
